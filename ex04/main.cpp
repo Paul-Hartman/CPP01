c7dda980fe03b5b7c80823e21ba5fa8b0e0cb21b
Paul Hartman <paul.hartman222@gmail.com>
2025-01-15 17:01:28 +0100

    somehwat working no input checks

--- ex04/main.cpp
+++ ex04/main.cpp
@@ -7,7 +7,10 @@ int main(int argc, char const *argv[])
 {
 	if(argc == 4)
 	{
-		std::ifstream file(argv[1]);
+		std::string filename = argv[1];
+		std::string str1 = argv[2];
+		std::string str2 = argv[3];
+		std::ifstream file(filename.c_str());
 		if(!file.is_open())
 		{
 			std::cout << "Error: could not open file" << std::endl;
@@ -16,10 +19,29 @@ int main(int argc, char const *argv[])
 		else
 		{
 			std::string line;
+			std::string newfilename = filename.append(".replace");
+			std::ofstream outfile(newfilename.c_str());
+			if(!outfile.is_open())
+			{
+				std::cout << "Error: could not open file" << std::endl;
+				return 1;
+			}
 			while(std::getline(file, line))
 			{
-				std::cout << line << std::endl;
+				std::size_t found = line.find(str1);
+				while(found != std::string::npos)
+				{
+					
+					line.insert(line.find(str1) + str1.length(), str2);
+					line.erase(line.find(str1), str1.length());
+					
+					found = line.find(str1);
+					
+				}
+				outfile << line << std::endl;
 			}
+			file.close();
+			outfile.close();
 		}
 			
 	}
